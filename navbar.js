050c5d942cf2debc76d1f26efa23ec995877c755
Victor Okeke <okekevicktur@gmail.com>
2020-06-09 01:28:40 +0100

    Initial Headers Commit

---
+++ navbar.js
@@ -0,0 +1,9 @@
+
+
+
+const harmburger = document.querySelector('.harmburger');
+const navbar = document.querySelector('.navbar');
+
+harmburger.addEventListener('click', ()=>{
+  navbar.classList.toggle('open');
+});
